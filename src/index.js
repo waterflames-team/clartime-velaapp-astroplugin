c5edaf8bf67f59fef5e547b42383d85e5d13a5a5
yulimfish <feifanxiaowang@163.com>
2025-07-17 17:31:14 +0800

    :construction: 数据传输落地，等待新插件系统测试上线

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import AstroBox from "astrobox-plugin-sdk";
-import Interconn from './interconn.ts';
-
-let AppInterconn
 let courseData
+let ui
 
 // UI服务启动
-const DataGetID = AstroBox.native.regNativeFun(DataGet);
-const ICSendId = AstroBox.native.regNativeFun(ICSend);
-
+let ICSendId = AstroBox.native.regNativeFun(ICSend);
 
 AstroBox.lifecycle.onLoad(() => {
-  AppInterconn = new Interconn("com.waterflames.clartime");
-  const ui = [
+  ui = [
     {
       node_id: "pickFile",
       visibility: true,
@@ -19,8 +14,8 @@ AstroBox.lifecycle.onLoad(() => {
       content: {
         type: "Input",
         value: {
-          text: "输入数据",
-          callback_fun_id: DataGetID
+          text: "你好",
+          callback_fun_id: ICSendId
         }
       }
     },
@@ -32,84 +27,19 @@ AstroBox.lifecycle.onLoad(() => {
         type: "Button",
         value: { primary: true, text: "发送", callback_fun_id: ICSendId },
       },
-    },
-    {
-      node_id: "Info",
-      visibility: true,
-      disabled: false,
-      content: {
-        type: "Text",
-        value: `等待中`
-      }
-    },
+    }
   ];
 
   AstroBox.ui.updatePluginSettingsUI(ui)
 });
 
-
-
-
-function DataGet(data) {
-  courseData = data;
-}
-
-// 数据获取
-/**
- * 获取文件数据
- * @returns {Promise<{pickRet: any, data: any}>} 返回文件选择结果和文件内容喵～
- */
-// async function DataGet() {
-//   try {
-//     // 1. 让用户选择文件
-//     const pickRet = await AstroBox.filesystem.pickFile();
-    
-//     // 2. 读取文件内容
-//     const data = await AstroBox.filesystem.readFile(pickRet.path);
-
-//     return data;
-//   } catch (error) {
-//     // 错误处理
-//     throw error;
-//   }
-// }
-
-// // 获取文件数据
-// const courseData = DataGet();
-
 // 数据传输
 async function ICSend() {
+  courseData = ui[0].content.value.text
 
   await AstroBox.interconnect.sendQAICMessage(
     "com.waterflames.clartime",
-    JSON.stringify({ cmd: "ping" })
+    JSON.stringify({ text: courseData })
   );
 
-  // AstroBox.ui.updatePluginSettingsUI(ui)
-  // try {
-  //   const appList = await AstroBox.thirdpartyapp.getThirdPartyAppList()
-  //   const app = appList.find(app => app.package_name == "com.waterflames.clartime")
-  //   if (!app) {
-  //     ui[2].content.value = "请先澄序课程表快应用"
-  //     //@ts-ignore
-  //     return AstroBox.ui.updatePluginSettingsUI(ui)
-  //   }
-  //   await AstroBox.thirdpartyapp.launchQA(app, "com.waterflames.clartime")
-  //   await new Promise(resolve => setTimeout(resolve, 1000))
-  //   if (AppInterconn) {
-  //     AppInterconn.send("CourseData", courseData);
-  //   } else {
-  //     console.error("AppInterconn 为 null，无法发送数据");
-  //     ui[2].content.value = "连接未初始化，无法发送数据";
-  //     //@ts-ignore
-  //     AstroBox.ui.updatePluginSettingsUI(ui);
-  //   }
-    
-  // } catch (error) {
-  //   console.error(error)
-  //   //@ts-ignore
-  //   ui[2].content.value = error.message
-  //   //@ts-ignore
-  //   AstroBox.ui.updatePluginSettingsUI(ui)
-  // }
 }
